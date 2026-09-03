8dc561e68998b779896a57affa99b48502c6d16a
Qixi1210 <meloneater_2023@qq.com>
2026-09-03 20:42:08 +0800

    Add files via upload

---
+++ js/members_page_onclick.js
@@ -0,0 +1,21 @@
+﻿document.getElementById("page_sgzz").onclick = function(){
+window.location.href = "./members/sgzz.html";
+}
+document.getElementById("page_windy").onclick = function(){
+window.location.href = "./members/windy.html";
+}
+document.getElementById("page_dove").onclick = function(){
+window.location.href = "./members/dove.html";
+}
+document.getElementById("page_asju").onclick = function(){
+window.location.href = "./members/asju.html";
+}
+document.getElementById("page_hs").onclick = function(){
+window.location.href = "./members/hs.html";
+}
+document.getElementById("page_wu").onclick = function(){
+window.location.href = "./members/wu.html";
+}
+document.getElementById("page_xhw").onclick = function(){
+window.location.href = "./members/xhw.html";
+}
