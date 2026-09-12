7020fff3309337dbe4cdf3a0d347872ea55edecf
Qixi1210 <meloneater_2023@qq.com>
2026-09-12 11:36:20 +0800

    Add files via upload

---
+++ js/project_page_onclick.js
@@ -0,0 +1,9 @@
+document.getElementById("phantomdream").onclick = function(){
+    window.location.href = "./project/phantomdream.html";
+}
+document.getElementById("jian_yu_yuan_zheng").onclick = function(){
+    window.location.href = "./project/jian_yu_yuan_zheng.html";
+}
+document.getElementById("love_you_fog_read").onclick = function(){
+    window.location.href = "./project/love_you_fog.html";
+}
